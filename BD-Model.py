1f564a321814281aa9d337b2b6a26fc4463cdd74
QuarkPlanks <1412580863@qq.com>
2025-07-12 15:22:07 +0800

    Add files via upload

---
+++ BD-Model.py
@@ -0,0 +1,170 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import sklearn 
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split, LeaveOneOut, cross_val_score
+import os
+os.chdir(os.path.dirname (os.path.realpath (__file__)))
+def model_train(X, y, model, display=False):
+    """
+    Train the model with the provided features and target variable.
+    
+    Parameters:
+    X (pd.DataFrame): Features for training.
+    y (pd.Series): Target variable for training.
+    model: The machine learning model to train.
+    
+    Returns:
+    model: The trained model.
+    """
+    model.fit(X, y)
+    loo = LeaveOneOut()
+    mse_scores = -cross_val_score(model, X, y, cv=loo, scoring='neg_mean_squared_error')
+    if display:
+        print(f"LOOCV MSE: {mse_scores.mean():.4f} ± {mse_scores.std():.4f}")
+    mse = mse_scores.mean()
+    return model, mse
+
+def model_validate(X, y, model):
+    """
+    Validate the model with the provided features and target variable.
+    
+    Parameters:
+    X (pd.DataFrame): Features for validation.
+    y (pd.Series): Target variable for validation.
+    model: The machine learning model to validate.
+    
+    Returns:
+    metrics: Dictionary containing R^2 and MSE of the validation.
+    """
+    predictions = model.predict(X)
+    r2 = r2_score(y, predictions)
+    mse = mean_squared_error(y, predictions)
+    print(f"Model validated with R^2: {r2:.4f}, MSE: {mse:.4f}")
+    metrics = {'r2': r2, 'mse': mse}
+    return predictions, metrics
+
+def plot_predictions(y_true, y_pred, title='Model Predictions'):
+    """
+    Plot the true vs predicted values.
+    
+    Parameters:
+    y_true (pd.Series): True target values.
+    y_pred (np.ndarray): Predicted values from the model.
+    title (str): Title of the plot.
+    """
+    plt.figure(figsize=(3, 3))
+    plt.scatter(y_true, y_pred, alpha=0.5)
+    plt.xlabel('True Values')
+    plt.ylabel('Predictions')
+    plt.title(title)
+    minimum = min(y_true.min(), y_pred.min())
+    maximum = max(y_true.max(), y_pred.max())
+    plt.plot([y_true.min(), y_true.max()], [y_true.min(), y_true.max()], 'r', lw=2)
+    plt.xlim(0.9*minimum, 1.1*maximum)
+    plt.ylim(0.9*minimum, 1.1*maximum)
+    plt.grid()
+    plt.show()
+
+def read_data(file_path):
+    """
+    Read data from a CSV file.
+    
+    Parameters:
+    file_path (str): Path to the CSV file.
+    
+    Returns:
+    pd.DataFrame: DataFrame containing the data.
+    """
+    os.chdir(r'data')
+    back = os.path.dirname(os.getcwd())
+    data = pd.read_excel(file_path)
+    x = np.array(data.drop(columns=['con.', 'BD']))
+    y_conversion = np.array(data['con.'])
+    y_BD = np.array(data['BD'])
+    os.chdir(back)
+    return x, y_conversion, y_BD
+
+# Import and process data
+from sklearn.preprocessing import StandardScaler
+
+train_x, train_y_con, train_y_BD = read_data('train_BD.xlsx')
+train_x[:,1] = np.exp(-1.3e4/(train_x[:,1]+273.15))
+test_x, test_y_con, test_y_BD = read_data('test_BD.xlsx')
+test_x[:,1] = np.exp(-1.3e4/(test_x[:,1]+273.15))
+scaler = StandardScaler()
+train_x_scaled = scaler.fit_transform(train_x)
+test_x_scaled = scaler.transform(test_x)
+
+# Models
+from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+from sklearn.svm import SVR
+from sklearn.neighbors import KNeighborsRegressor
+from xgboost import XGBRegressor
+from sklearn.neural_network import MLPRegressor
+from sklearn.ensemble import RandomForestRegressor
+Linear =  LinearRegression(fit_intercept=True)
+Ridge = model = Ridge(alpha=1)
+Quadratic = make_pipeline(PolynomialFeatures(degree=2), LinearRegression())
+SVM = SVR(kernel='rbf', C=1.0, epsilon=0.1)
+kNN = KNeighborsRegressor(n_neighbors=3, weights='distance')
+XGBoost = XGBRegressor(n_estimators=20, max_depth=2, learning_rate=0.1)
+Neural = MLPRegressor(
+    hidden_layer_sizes=(3,), 
+    activation='relu',
+    solver='lbfgs',          
+    alpha=0.5,             
+    max_iter=300,
+    random_state=42
+)
+RandomForest = RandomForestRegressor(n_estimators=10, max_depth=2, random_state=42)
+
+# Model Training
+trained_models = {}
+model_names = ['Linear', 'Ridge', 'Quadratic', 'SVM', 'kNN', 'XGBoost', 'Neural', 'RandomForest']
+models = [Linear, Ridge, Quadratic, SVM, kNN, XGBoost, Neural, RandomForest]
+for model, name in zip(models, model_names):
+    print(f"Training {name} model...")
+    trained_model, mse = model_train(train_x_scaled, train_y_BD, model)
+    trained_models[name] = trained_model
+    print(f"{name} model trained with MSE: {mse:.4f}")
+
+# Plot
+for name, model in trained_models.items():
+    print(f"Validating {name} model...")
+    predictions, metrics = model_validate(test_x_scaled, test_y_BD, model)
+    plot_predictions(test_y_BD, predictions, title=f'{name} Model Predictions')
+    print(f"{name} model validation metrics: {metrics}")
+    print("=" * 50)
+
+import pickle as pkl
+def save_model(model, filename):
+    """
+    Save the trained model to a file.
+    
+    Parameters:
+    model: The trained model to save.
+    filename (str): The name of the file to save the model to.
+    """
+    with open(filename, 'wb') as f:
+        pkl.dump(model, f)
+    print(f"Model saved to {filename}")
+
+# Save Model
+import os
+train_x, train_y_con, train_y_BD = read_data('BD.xlsx')
+train_x[:,1] = np.exp(-1e4/(train_x[:,1]+273.15))
+train_x_scaled = scaler.fit_transform(train_x)
+model_names = ['Linear']
+models = [Linear]
+os.chdir(r'model')
+for model, name in zip(models, model_names):
+    print(f"Training {name} model...")
+    trained_model, mse = model_train(train_x_scaled, train_y_BD, model)
+    trained_models[name] = trained_model
+    print(f"{name} model trained with MSE: {mse:.4f}")
+save_model(trained_models['Linear'], 'BD.pkl')
+save_model(scaler, 'scaler_BD.pkl')
---
+++ Designer.py
@@ -0,0 +1,116 @@
+import numpy as np
+import scipy
+import pandas as pd
+import matplotlib.pyplot as plt
+import pickle as pkl
+def data_process_con(x):
+    df = pd.DataFrame(x)
+    df[5] = 10*x[:,2]*x[:,3]*x[:,4]*np.exp(-1000*(1/(x[:,1]+273.15)-1/(40+273.15)))/(1+10*x[:,2]*x[:,3]*x[:,4]*np.exp(-1000*(1/(x[:,1]+273.15)-1/(40+273.15))))
+    return np.array(df)
+
+# Import Models
+import os
+os.chdir(os.path.dirname (os.path.realpath (__file__)))
+os.chdir(r'model')
+def load_model(filename):
+    """
+    Load a trained model from a file.
+    
+    Parameters:
+    filename (str): The name of the file to load the model from.
+    
+    Returns:
+    model: The loaded model.
+    """
+    with open(filename, 'rb') as f:
+        model = pkl.load(f)
+    print(f"Model loaded from {filename}")
+    return model
+
+con_model = load_model('conversion.pkl')
+scaler_con = load_model('scaler_con.pkl')
+BD_model = load_model('BD.pkl')
+scaler_BD = load_model('scaler_BD.pkl')
+
+# Optimizer
+def objective(x):
+    x = x.reshape(1, -1)  
+    x = data_process_con(x)
+    x = scaler_con.transform(x)
+    y = con_model.predict(x)
+    if y >= 100:
+        y = 100
+    elif y <= 0:
+        y = 0
+    return np.clip(y, -1e4, 1e4)
+
+def restrict(x):
+    x = x.reshape(1, -1)  
+    xx = x.copy()
+    xx[:,1] = np.exp(-1.3e4/(xx[:,1]+273.15))
+    xx = scaler_BD.transform(xx)
+    y = BD_model.predict(xx)
+    if y <= 0:
+        y = 0
+    return np.clip(y, -1e4, 1e4)
+
+from scipy.optimize import minimize
+def optimization(BD, disp=False, search=False):
+    cons = (
+        {'type': 'ineq', 'fun': lambda x: restrict(x) - (BD-5)}, # restrict(x) >= BD-5
+        {'type': 'ineq', 'fun': lambda x: - restrict(x) + (BD+5)}, # restrict(x) <= BD+5
+        {'type': 'ineq', 'fun': lambda x: x[0] - 0.1}, # pressure >= 0.1
+        {'type': 'ineq', 'fun': lambda x: - x[0] + 1.3}, # pressure <= 1.3
+        {'type': 'ineq', 'fun': lambda x: x[1] + 5}, # temperature >= -5 ℃
+        {'type': 'ineq', 'fun': lambda x: - x[1] + 30}, # temperature <= 30 ℃
+        {'type': 'ineq', 'fun': lambda x: x[2] - 5}, # time >= 5 min
+        {'type': 'ineq', 'fun': lambda x: - x[2] + 17}, # time <= 20 min
+        {'type': 'ineq', 'fun': lambda x: x[3] - 20}, # q_Ethylene >= 20
+        {'type': 'ineq', 'fun': lambda x: - x[3] + 55}, # q_Ethylene <= 55
+        {'type': 'ineq', 'fun': lambda x: x[4] - 0.004}, # c_catalyst >= 0.004
+        {'type': 'ineq', 'fun': lambda x: - x[4] + 0.025}, # c_catalyst <= 0.025
+        {'type': 'ineq', 'fun': lambda x: objective(x) - 10}, # conversion >= 10%
+        {'type': 'ineq', 'fun': lambda x: - objective(x) + 95} # conversion <= 95%
+    )
+
+    fun = lambda x: -objective(x)
+    temp_con = -1
+    best_res = None
+    if search:
+        print("Searching for the best parameters...")
+        for p in [0.1, 0.5, 0.7, 1.2]:
+            for T in [-3,5,15,25]:
+                for t in [10,15]:
+                    for q in [25,35,45]:
+                        for c in [0.006, 0.011, 0.016, 0.021]:
+                            x0 = np.array([p, T, t, q, c])
+                            res = minimize(fun, x0, constraints=cons, method='SLSQP')
+                            if res.success and objective(res.x) > temp_con + 0.001:
+                                temp_con = objective(res.x)
+                                best_res = res
+    else:
+        print("Optimizing parameters...")
+        x0 = np.array([0.8, 10, 15, 45, 0.0122])
+        best_res = minimize(fun, x0, constraints=cons, method='SLSQP', options={'maxiter': 1000, 'ftol': 1e-6})
+    if disp:
+        if best_res is None:
+            print("No feasible solution found.")
+            return None
+        else:
+            res = best_res
+            print('Success:', res.success)
+            print('=' * 30)
+            print('Parameters:')
+            print('Pressure(MPa):', res.x[0])
+            print('Temperature(℃):', res.x[1])
+            print('Time(min):', res.x[2])
+            print('q_Ethylene(sccm):', res.x[3])
+            print('c_catalyst(g/100mL):', res.x[4])
+            print('=' * 30)
+            print("Results:")
+            print('BD:', restrict(res.x))
+            print('Conversion(%):', objective(res.x))
+            return res
+
+# Design
+res = optimization(125, True, True)
